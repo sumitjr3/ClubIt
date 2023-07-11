803c5e6feb387b67608c50a7683ecf375f960938
sumitjr3 <103193051+sumitjr3@users.noreply.github.com>
2023-07-11 11:50:00 +0530

    HOHO

--- app/src/main/java/com/example/clubit/MainActivity.java
+++ app/src/main/java/com/example/clubit/MainActivity.java
@@ -1,14 +1,64 @@
 package com.example.clubit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    private BottomNavigationView bottomNavigationView;
+    private FrameLayout frameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
+        frameLayout = findViewById(R.id.frameLayout);
+
+        bottomNavigationView.inflateMenu(R.menu.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+
+        loadFragment(new HomeFragment());
     }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.frameLayout, fragment);
+        ft.commit();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment selectedFragment;
+
+        int itemId = item.getItemId();
+        if (itemId == R.id.home) {
+            selectedFragment = new HomeFragment();
+        } else if (itemId == R.id.club) {
+            selectedFragment = new ClubFragment();
+        } else if (itemId == R.id.create) {
+            selectedFragment = new CreateFragment();
+        } else if (itemId == R.id.leaderBoard) {
+            selectedFragment = new LeaderboardFragment();
+        } else if (itemId == R.id.profile) {
+            selectedFragment = new profileFragment();
+        } else {
+            return false;
+        }
+
+        loadFragment(selectedFragment);
+        return true;
+    }
+
 }
