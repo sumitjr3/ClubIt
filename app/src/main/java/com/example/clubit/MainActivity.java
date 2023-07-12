214f4b0f99220b0636cbb23c71e9cc0c8656ca9e
sumitjr3 <103193051+sumitjr3@users.noreply.github.com>
2023-07-12 12:16:21 +0530

    conflicts resolved

--- app/src/main/java/com/example/clubit/MainActivity.java
+++ app/src/main/java/com/example/clubit/MainActivity.java
@@ -3,9 +3,6 @@ package com.example.clubit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.FrameLayout;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
